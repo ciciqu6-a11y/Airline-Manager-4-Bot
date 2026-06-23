6abaf8cdb4e1cff88f259b6e18d872ecf20856f1
elssword4587 <elssword4587@gmail.com>
2026-06-23 09:42:21 +0700

    Update fuel.utils.ts

--- utils/fuel.utils.ts
+++ utils/fuel.utils.ts
@@ -1,11 +1,11 @@
 import { Page } from "@playwright/test";
+import { GeneralUtils } from "./general.utils";
 
 require('dotenv').config();
 
 export class FuelUtils {
     maxFuelPrice : number;
     maxCo2Price : number;
-
     page : Page;
 
     constructor(page : Page) {
@@ -23,25 +23,23 @@ export class FuelUtils {
         const getCurrentFuelPrice = async () => {
             let fuelText = await this.page.getByText('Total price$').locator('b > span').innerText();
             fuelText = fuelText.replaceAll(',', '');
-            
             return parseInt(fuelText);
         }
 
         const getCurrentHolding = async () => {
             let holdingText = await this.page.locator('#holding').innerText();
             holdingText = holdingText.replaceAll(',', '');
-
             return parseInt(holdingText);
         }
 
         const getEmptyFuel = async () => {
             const emptyText = (await this.page.locator('#remCapacity').innerText()).replaceAll(',', '')
-
             return parseInt(emptyText);
         }
 
         const emptyFuel = await getEmptyFuel();
         if(emptyFuel === 0) {
+            console.log('Fuel tank is already full.');
             return;
         }
 
@@ -50,52 +48,64 @@ export class FuelUtils {
 
         console.log('Current Fuel Price: ' + curFuelPrice);
 
-        // Buy fuel if current price is lower than max price
+        // Beli bensin jika harga di bawah batas maksimum yang ditentukan pengguna
         if(curFuelPrice < this.maxFuelPrice) {
             const emptyFuelCapacity = (await this.page.locator('#remCapacity').innerText()).replaceAll(',', '');
 
-            await this.page.getByPlaceholder('Amount to purchase').click();
+            // KOREKSI: Ganti ke humanClick dan gunakan simulasi mengetik lambat
+            await GeneralUtils.humanClick(this.page, this.page.getByPlaceholder('Amount to purchase'));
+            await GeneralUtils.randomSleep(500, 1200);
+            
             await this.page.getByPlaceholder('Amount to purchase').press('Control+a');
-            await this.page.getByPlaceholder('Amount to purchase').fill(emptyFuelCapacity);
-            await this.page.getByRole('button', { name: ' Purchase' }).click();
+            await GeneralUtils.randomSleep(400, 900);
+            
+            await this.page.getByPlaceholder('Amount to purchase').pressSequentially(emptyFuelCapacity, { delay: Math.floor(Math.random() * 80) + 40 });
+            await GeneralUtils.randomSleep(1000, 2000);
+            
+            await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: ' Purchase' }));
 
             console.log('Bought Fuel Successfully! Amount of fuel bought: ' + emptyFuelCapacity + ' Litres');
         }
+        // Kondisi darurat jika stok kritis (< 2M) meskipun harga agak mahal
         else if(curHolding < 2000000 && curFuelPrice < 1250) {
-            const emptyFuelCapacity = (await this.page.locator('#remCapacity').innerText()).replaceAll(',', '');
-
-            await this.page.getByPlaceholder('Amount to purchase').click();
+            await GeneralUtils.humanClick(this.page, this.page.getByPlaceholder('Amount to purchase'));
+            await GeneralUtils.randomSleep(500, 1200);
+            
             await this.page.getByPlaceholder('Amount to purchase').press('Control+a');
-            await this.page.getByPlaceholder('Amount to purchase').fill('2000000');
-            await this.page.getByRole('button', { name: ' Purchase' }).click();
+            await GeneralUtils.randomSleep(400, 900);
+            
+            await this.page.getByPlaceholder('Amount to purchase').pressSequentially('2000000', { delay: Math.floor(Math.random() * 80) + 40 });
+            await GeneralUtils.randomSleep(1000, 2000);
+            
+            await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: ' Purchase' }));
 
-            console.log('Bought Fuel Successfully! Amount of fuel bought: 2000000 Litres');
+            console.log('Bought Fuel Successfully! Amount of fuel bought: 2000000 Litres (Emergency Buy)');
         } 
     }
 
     public async buyCo2() {
+        console.log('Buying CO2...')
+
         const getCurrentCo2Price = async () => {
             let co2Text = await this.page.getByText('Total price$').locator('b > span').innerText();
             co2Text = co2Text.replaceAll(',', '');
-            
             return parseInt(co2Text);
         }
 
         const getCurrentHolding = async () => {
             let holdingText = await this.page.locator('#holding').innerText();
             holdingText = holdingText.replaceAll(',', '');
-
             return parseInt(holdingText);
         }
 
         const getEmptyCO2 = async () => {
             const emptyText = (await this.page.locator('#remCapacity').innerText()).replaceAll(',', '')
-
             return parseInt(emptyText);
         }
 
         const emptyCo2 = await getEmptyCO2();
         if(emptyCo2 === 0) {
+            console.log('CO2 tank is already full.');
             return;
         }
 
@@ -104,24 +114,38 @@ export class FuelUtils {
 
         console.log('Current Co2 Price: ' + curCo2Price);
 
-        // Buy co2 if current price is lower than max price
+        // Beli CO2 jika harga di bawah harga maksimum target
         if(curCo2Price < this.maxCo2Price) {
             const emptyCo2Capacity = (await this.page.locator('#remCapacity').innerText()).replaceAll(',', '');
 
-            await this.page.getByPlaceholder('Amount to purchase').click();
+            // KOREKSI: Ganti ke humanClick dan pressSequentially
+            await GeneralUtils.humanClick(this.page, this.page.getByPlaceholder('Amount to purchase'));
+            await GeneralUtils.randomSleep(500, 1200);
+            
             await this.page.getByPlaceholder('Amount to purchase').press('Control+a');
-            await this.page.getByPlaceholder('Amount to purchase').fill(emptyCo2Capacity);
-            await this.page.getByRole('button', { name: ' Purchase' }).click();
+            await GeneralUtils.randomSleep(400, 900);
+            
+            await this.page.getByPlaceholder('Amount to purchase').pressSequentially(emptyCo2Capacity, { delay: Math.floor(Math.random() * 80) + 40 });
+            await GeneralUtils.randomSleep(1000, 2000);
+            
+            await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: ' Purchase' }));
 
             console.log('Bought Co2 Successfully! Amount of co2 bought: ' + emptyCo2Capacity);
         }
+        // Kondisi darurat jika stok kuota emisi kritis (< 1M)
         else if(curHolding < 1000000 && curCo2Price < 180) {
-            await this.page.getByPlaceholder('Amount to purchase').click();
+            await GeneralUtils.humanClick(this.page, this.page.getByPlaceholder('Amount to purchase'));
+            await GeneralUtils.randomSleep(500, 1200);
+            
             await this.page.getByPlaceholder('Amount to purchase').press('Control+a');
-            await this.page.getByPlaceholder('Amount to purchase').fill('1000000');
-            await this.page.getByRole('button', { name: ' Purchase' }).click();
+            await GeneralUtils.randomSleep(400, 900);
+            
+            await this.page.getByPlaceholder('Amount to purchase').pressSequentially('1000000', { delay: Math.floor(Math.random() * 80) + 40 });
+            await GeneralUtils.randomSleep(1000, 2000);
+            
+            await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: ' Purchase' }));
 
-            console.log('Bought Co2 Successfully! Amount of co2 bought: 1000000');
+            console.log('Bought Co2 Successfully! Amount of co2 bought: 1000000 (Emergency Buy)');
         }
     }
 }
