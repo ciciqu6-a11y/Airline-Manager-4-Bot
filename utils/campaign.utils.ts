cc6a70c0b3d1ac7cbb5705b1ca99bd61411fe81b
elssword4587 <elssword4587@gmail.com>
2026-06-23 13:17:02 +0700

    Update campaign.utils.ts

--- utils/campaign.utils.ts
+++ utils/campaign.utils.ts
@@ -3,6 +3,7 @@ import { GeneralUtils } from "./general.utils";
 
 export class CampaignUtils {
     page: Page;
+
     increaseAirlineReputation: boolean = false;
     campaignType: number = 1;
     campaignDuration: number = 4;
@@ -13,19 +14,22 @@ export class CampaignUtils {
             this.campaignType = parseInt(process.env.CAMPAIGN_TYPE!);
             this.campaignDuration = parseInt(process.env.CAMPAIGN_DURATION!);
         }
+
         this.page = page;
     }
 
     private async createEcoFriendly() {
-        const isEcoFriendExists = await this.page.getByRole('cell', { name: 'Eco friendly', exact: false }).isVisible();
+        const isEcoFriendExists = await this.page.getByRole('cell', { name: ' Eco friendly' }).isVisible();
         if(!isEcoFriendExists) {
-            await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: 'New campaign', exact: false }));
+            // KOREKSI: Ganti ke humanClick
+            await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: ' New campaign' }));
             await GeneralUtils.randomSleep(1000, 2000);
             
-            await GeneralUtils.humanClick(this.page, this.page.getByRole('cell', { name: 'Eco-friendly Increases', exact: false }));
+            await GeneralUtils.humanClick(this.page, this.page.getByRole('cell', { name: 'Eco-friendly Increases' }));
             await GeneralUtils.randomSleep(1000, 2000);
             
             await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: '$' }));
+
             console.log("Eco Friendly Campaign Created Successfully!");
         }
     }
@@ -34,31 +38,39 @@ export class CampaignUtils {
         const campaignType = this.campaignType.toString();
         const durationOption = (Math.floor(this.campaignDuration / 4) || 1).toString();
 
-        const isAirlineReputationExists = await this.page.getByRole('cell', { name: 'Airline reputation', exact: false }).isVisible();
+        const isAirlineReputationExists = await this.page.getByRole('cell', { name: ' Airline reputation' }).isVisible();
         if (!isAirlineReputationExists) {
-            await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: 'New campaign', exact: false }));
+            // KOREKSI: Ganti ke humanClick
+            await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: ' New campaign' }));
             await GeneralUtils.randomSleep(1000, 2000);
             
-            await GeneralUtils.humanClick(this.page, this.page.getByRole('cell', { name: 'Increase airline reputation', exact: false }));
+            await GeneralUtils.humanClick(this.page, this.page.getByRole('cell', { name: 'Increase airline reputation' }));
             await GeneralUtils.randomSleep(1000, 2000);
             
             await this.page.locator('#dSelector').selectOption(durationOption);
             await GeneralUtils.randomSleep(1000, 2000);
             
             await GeneralUtils.humanClick(this.page, this.page.locator(`tr:has(td:has-text("Campaign ${campaignType}")) .btn-danger`));
+
             console.log("Increased Airline Reputation Successfully!");
         }
     }
 
     public async createCampaign() {
-        console.log('Create Campaign Started...');
-        await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: 'Marketing', exact: false }));
+        console.log('Create Campaign Started...')
+
+        // KOREKSI: Ganti ke humanClick
+        await GeneralUtils.humanClick(this.page, this.page.getByRole('button', { name: ' Marketing' }));
+
         await GeneralUtils.randomSleep(1500, 3000);
+
         await this.createEcoFriendly();
         await GeneralUtils.randomSleep(1500, 3000);
+
         if(this.increaseAirlineReputation) {
             await this.createReputation();
         }
+
         console.log('Campaign Created Finished!');
     }
 }
