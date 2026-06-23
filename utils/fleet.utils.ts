0d15fc70cd030367343add97a4ad9fa090b9bb45
elssword4587 <elssword4587@gmail.com>
2026-06-23 13:18:22 +0700

    Update fleet.utils.ts

--- utils/fleet.utils.ts
+++ utils/fleet.utils.ts
@@ -20,6 +20,7 @@ export class FleetUtils {
 
             let departAll = this.page.locator('#departAll');
             
+            // KOREKSI 4: Tunggu respons API rute penerbangan asli selesai diproses jaringan
             await Promise.all([
                 this.page.waitForResponse(response => 
                     response.url().includes('route') && response.status() === 200, 
@@ -28,6 +29,7 @@ export class FleetUtils {
                 GeneralUtils.humanClick(this.page, departAll)
             ]);
 
+            // Tambahkan jeda santai manusia pasca-klik (proses berpikir/animasi)
             await GeneralUtils.randomSleep(1500, 3000);
             
             const cantDepartPlane = await this.page.getByText('×Unable to departSome A/C was').isVisible();
